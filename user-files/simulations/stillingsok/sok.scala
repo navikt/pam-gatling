424a3df865e696b5522ef4f502ac5b5d2e3c66f1
erlendjohansen <erlend.johansen@nav.no>
2018-06-01 13:23:37 +0200

    Endrer miljø fra prod til test. Splitter testen i flere objects.

--- user-files/simulations/stillingsok/sok.scala
+++ user-files/simulations/stillingsok/sok.scala
@@ -5,17 +5,18 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
-import scala.concurrent.duration._
 
 class Stillingsok extends Simulation {
 
   val httpProtocol = http
-    .baseURL("https://pam-stillingsok.nais.oera.no")
+    .baseURL("https://pam-stillingsok.nais.oera-q.local")
     .inferHtmlResources(BlackList(""".*\.css""", """.*\.js""", """.*\.ico"""), WhiteList())
     .acceptHeader("*/*")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.9")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.64")
+
+  val uri1 = "https://pam-stillingsok.nais.oera-q.local:443"
 
   val headers_0 = Map(
     "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -23,23 +24,25 @@ class Stillingsok extends Simulation {
 
   val headers_1 = Map(
     "content-type" -> "application/json",
-    "origin" -> "https://pam-stillingsok.nais.oera.no")
+    "origin" -> "https://pam-stillingsok.nais.oera-q.local")
 
-  val uri1 = "https://pam-stillingsok.nais.oera.no:443"
+  val searchInput = csv("yrker.csv").random
+
+  object FrontPage {
+
+    val loadFrontPage = exec(http("request_get_front_page")
+      .get("/")
+      .headers(headers_0)
+      .resources(http("request_initial_search")
+        .post("/pam-stillingsok/search-api/ad/_search")
+        .headers(headers_1)
+        .body(ElFileBody("stillingsok_request_initial_search.txt"))))
+      .pause(5)
+  }
 
   object Search {
 
-    val searchInput = csv("yrker.csv").random
-
     val search = feed(searchInput)
-      .exec(http("request_get_front_page")
-        .get("/")
-        .headers(headers_0)
-        .resources(http("request_initial_search")
-          .post("/pam-stillingsok/search-api/ad/_search")
-          .headers(headers_1)
-          .body(ElFileBody("stillingsok_request_initial_search.txt"))))
-      .pause(5)
       .exec(http("request_typeahead")
         .post("/pam-stillingsok/search-api/ad/_search")
         .headers(headers_1)
@@ -50,7 +53,7 @@ class Stillingsok extends Simulation {
         .headers(headers_1)
         .body(ElFileBody("stillingsok_request_search.txt"))
         .check(jsonPath("$.timed_out").is("false")))
-//        .check(jsonPath("$").saveAs("response"))
+//        .check(jsonPath("$").saveAs("response")))
 //      .exec(session => { // Print respons
 //        println("request_search:")
 //        println(session("response").as[String])
@@ -58,10 +61,10 @@ class Stillingsok extends Simulation {
 //      })
   }
 
-  val search = scenario("Search").exec(Search.search)
+  val search = scenario("Search").exec(FrontPage.loadFrontPage, Search.search)
 
   setUp(
-    //search.inject(atOnceUsers(500))
+    //search.inject(atOnceUsers(1))
     search.inject(rampUsers(1000) over (10 seconds))
   ).protocols(httpProtocol)
 }
